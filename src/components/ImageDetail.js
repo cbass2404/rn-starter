486bdeb512797b69e64b0eaaa7a470f40f4bccd6
Cory Bass <cory.bass.07@gmail.com>
2021-03-30 14:01:41 -0500

    added scores to images in ImageScreen.js

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -2,11 +2,12 @@ import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 
 const ImageDetail = (props) => {
-    const { title, imageSource } = props;
+    const { title, imageSource, imageScore } = props;
     return (
         <View>
             <Image source={imageSource} />
             <Text>{title}</Text>
+            <Text>Rating {imageScore}</Text>
         </View>
     );
 };
