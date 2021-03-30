64bca0e77b530fe217264eef856dc7bed3c14f7f
Cory Bass <cory.bass.07@gmail.com>
2021-03-30 13:41:27 -0500

    added titles to ImageScreen

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ImageDetail = (props) => {
+    const { title } = props;
+    return <Text>{title}</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageDetail;
