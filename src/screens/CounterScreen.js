b55e0618a716cc9fd04d9b143116dd4f83464188
Cory Bass <cory.bass.07@gmail.com>
2021-04-09 13:03:08 -0500

    refactored counterscreen.js with reducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,14 +1,40 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
+const COUNT_INCREMENT = 10;
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "add_number":
+            return { ...state, count: state.count + action.payload };
+        case "subtract_number":
+            return { ...state, count: state.count + action.payload };
+        default:
+            return state;
+    }
+};
+
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, { count: 0 });
 
     return (
         <View>
-            <Button title="Increase" onPress={() => setCounter(counter + 1)} />
-            <Button title="Decrease" onPress={() => setCounter(counter - 1)} />
-            <Text>Current Count: {counter}</Text>
+            <Button
+                title="Increase"
+                onPress={() =>
+                    dispatch({ type: "add_number", payload: COUNT_INCREMENT })
+                }
+            />
+            <Button
+                title="Decrease"
+                onPress={() =>
+                    dispatch({
+                        type: "subtract_number",
+                        payload: COUNT_INCREMENT * -1,
+                    })
+                }
+            />
+            <Text>Current Count: {state.count}</Text>
         </View>
     );
 };
